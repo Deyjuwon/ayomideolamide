805184622f8fc380fcf22327d21a2243b10594e9
Deyjuwon <temitayoadejuwon@gmail.com>
2024-05-18 23:43:15 +0100

    contact page created

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,25 @@
-
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaTiktok } from "react-icons/fa";
 
 const Contact = () => {
   return (
-    <div>
-      contatc
+    <div className="flex flex-col justify-center text-center items-center pt-40">
+      <div className="border-t-2 w-4/5 pb-7"></div>
+      <div className="pb-7 text-gray-500"> 
+        <p className="montserrat pb-6 text-black tracking-widerr">CONTACTS</p>
+        <p className="text-sm">ayomideolamide@yahoo.com</p>
+        <p className="text-sm">info@ayomideola.com</p>
+        <div className="pb-6"></div>
+        <div className="flex gap-5 justify-center items-center">
+          <a href="https://www.instagram.com/ajayiayomideo/instagram.com" target="_blank"><FaInstagram size={25} /></a>
+          <a href="https://www.twitter.com"><FaXTwitter size={25} /></a>
+          <a href="https://www.tiktok.com"><FaTiktok size={25} /></a>
+
+        </div>
+
+      </div>
+      <div className="border-t-2 w-4/5"></div>
     </div>
   )
 }
