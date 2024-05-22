dce95ef7780c7fa4d43d3bfbe34e204fae1f2842
Deyjuwon <temitayoadejuwon@gmail.com>
2024-05-22 23:28:39 +0100

    changed footer font

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div className="news-cycle text-center font-normal text-xs text-gray-500">
+    <div className=" text-center font-normal text-xs text-gray-500">
         <p>&copy; 2024 Ayomide Olamide all rights reserved</p>
       
     </div>
