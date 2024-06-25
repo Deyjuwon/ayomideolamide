bedeb2a14c9695787f3de7db100f9df436a97621
Deyjuwon <temitayoadejuwon@gmail.com>
2024-06-25 16:44:11 +0100

    adding images

--- src/utils/SportImgs.js
+++ src/utils/SportImgs.js
@@ -1,18 +1,70 @@
-import b2b1 from '../assets/back2base/DSC00081.jpg';
-import b2b2 from '../assets/back2base/DSC00600.jpg';
-import b2b3 from '../assets/back2base/DSC00639.jpg';
-import b2b4 from '../assets/back2base/DSC00650.jpg';
-import b2b5 from '../assets/back2base/DSC00862.jpg';
-import b2b6 from '../assets/back2base/DSC00867.jpg';
-import b2b7 from '../assets/back2base/DSC00888.jpg';
-import b2b8 from '../assets/back2base/DSC01189.jpg';
-import b2b9 from '../assets/back2base/DSC01211.jpg';
-import b2b10 from '../assets/back2base/DSC01226.jpg';
-import b2b11 from '../assets/back2base/DSC01242.jpg';
-import b2b12 from '../assets/back2base/DSC01243.jpg';
-import b2b13 from '../assets/back2base/DSC01254.jpg';
+import sossa4 from '../assets/sossa/Untitled-2252.jpg';
+import sossa5 from '../assets/sossa/Untitled-2253.jpg';
+import sossa6 from '../assets/sossa/Untitled-2254.jpg';
+import sossa7 from '../assets/sossa/Untitled-2255.jpg';
+import sossa8 from '../assets/sossa/Untitled-2256.jpg';
+import sossa9 from '../assets/sossa/Untitled-2258.jpg';
+import sossa10 from '../assets/sossa/Untitled-2260.jpg';
+import sossa11 from '../assets/sossa/Untitled-2261.jpg';
+
+import b2b1 from '../assets/back2base/DSC00081.webp';
+import b2b2 from '../assets/back2base/DSC00600.webp';
+import b2b3 from '../assets/back2base/DSC00639.webp';
+import b2b4 from '../assets/back2base/DSC00650.webp';
+import b2b5 from '../assets/back2base/DSC09991.webp';
+import b2b6 from '../assets/back2base/DSC00867.webp';
+import b2b7 from '../assets/back2base/DSC00888.webp';
+import b2b8 from '../assets/back2base/DSC01189.webp';
+import b2b11 from '../assets/back2base/DSC01242.webp';
+import b2b12 from '../assets/back2base/DSC01243.webp';
+import b2b13 from '../assets/back2base/DSC01254.webp';
 
 const SportImgs = [
+    {
+      imgUrl: sossa1,
+      text: "sossa sports week",
+    },
+    {
+      imgUrl: sossa2,
+      text: "sossa sports week",
+    },
+    {
+      imgUrl: sossa3,
+      text: "sossa sports week",
+    },
+    {
+      imgUrl: sossa4,
+      text: "sossa sports week",
+    },
+    {
+      imgUrl: sossa5,
+      text: "sossa sports week",
+    },
+    {
+      imgUrl: sossa6,
+      text: "sossa sports week",
+    },
+    {
+      imgUrl: sossa7,
+      text: "sossa sports week",
+    },
+    {
+      imgUrl: sossa8,
+      text: "sossa sports week",
+    },
+    {
+      imgUrl: sossa9,
+      text: "sossa sports week",
+    },
+    {
+      imgUrl: sossa10,
+      text: "sossa sports week",
+    },
+    {
+      imgUrl: sossa11,
+      text: "sossa sports week",
+    },
+
     
     {
       imgUrl: b2b1,
@@ -44,14 +96,6 @@ const SportImgs = [
     },
     {
       imgUrl: b2b8,
-      text: "B2B tournament image",
-    },
-    {
-      imgUrl: b2b9,
-      text: "B2B tournament image",
-    },
-    {
-      imgUrl: b2b10,
       text: "B2B tournament image",
     },
     {
