d4b59044bb3ffb16a01540f6829e93f105882320
Deyjuwon <temitayoadejuwon@gmail.com>
2024-12-20 01:38:38 +0100

    SEO

--- src/pages/Event.jsx
+++ src/pages/Event.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 import Modal from "react-modal";
 import Header from "../components/Header";
 import data from "../utils/EventImgs";
